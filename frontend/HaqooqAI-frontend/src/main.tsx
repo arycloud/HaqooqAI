0fd170dd581875113f4848d61b73baba2458dd4a
Abdul Rehman <abdul12391@gmail.com>
2025-08-26 13:50:22 +0500

    Added custom domain file

--- frontend/HaqooqAI-frontend/src/main.tsx
+++ frontend/HaqooqAI-frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { Toaster } from 'react-hot-toast'
@@ -33,7 +34,7 @@ persistQueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/HaqooqAI">
+      <HashRouter basename="/HaqooqAI">
         <App />
         <Toaster 
           position="top-right"
@@ -59,7 +60,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             },
           }}
         />
-      </BrowserRouter>
+      </HashRouter>
       {/* Enable React Query Devtools
       <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
